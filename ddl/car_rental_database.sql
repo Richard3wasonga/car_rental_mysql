3d571c8d7ff83cca9e636ec67b0d802806c6365c
Richard Wasonga <rowwasonga@gmail.com>
2026-08-06 22:28:40 +0300

    Format Employees foreign key constraint

--- ddl/car_rental_database.sql
+++ ddl/car_rental_database.sql
@@ -18,8 +18,10 @@ CREATE TABLE Employees (
     hire_date DATE NOT NULL,
     branch_id INT NOT NULL,
     
-    CONSTRAINT fk_employee_branch FOREIGN KEY (branch_id) REFERENCES Branches(branch_id)
-    ON UPDATE CASCADE 
-    ON DELETE RESTRICT 
+    CONSTRAINT fk_employee_branch
+        FOREIGN KEY (branch_id) 
+        REFERENCES Branches(branch_id)
+        ON UPDATE CASCADE 
+        ON DELETE RESTRICT 
 );
 
