5ccfc3b75765da9b11eb39501aa815dfe15955e4
Joexvs <joendungu47@gmail.com>
2026-08-06 20:47:48 +0300

    Updated car_rental_database.sql
    Added the Employee table

--- ddl/car_rental_database.sql
+++ ddl/car_rental_database.sql
@@ -3,3 +3,22 @@
 CREATE DATABASE IF NOT EXISTS driveease_rentals;
 
 USE driveease_rentals;
+
+create table Branches(
+branch_id int primary key
+);
+
+CREATE TABLE Employees (
+    employee_id INT AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    phone VARCHAR(20) NOT NULL UNIQUE,
+    job_title VARCHAR(50) NOT NULL,
+    hire_date DATE NOT NULL,
+    branch_id INT NOT NULL,
+
+    FOREIGN KEY (branch_id)
+        REFERENCES Branches(branch_id)
+);
+
